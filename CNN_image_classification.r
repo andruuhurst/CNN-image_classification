9be29533b47e7c1b738183eda7726463901d5990
Andrew Hurst <andrew_hurst92@yahoo.com>
2020-05-01 23:50:03 -0700

    Accuracy of models on test data

--- CNN_image_classification.r
+++ CNN_image_classification.r
@@ -19,24 +19,25 @@ zip.class.tab <- table(zip.train$V1)
 num_classes <- length(zip.class.tab)
 zip.y.mat <- keras::to_categorical(zip.train$V1,num_classes)
 batch_size <- 128
-epochs <- 20
+epochs <- 30
 
 
 set.seed(1)
 fold.vec <- sample(rep(1:5, l=nrow(zip.train)))
 test.fold <- 1
 is.test <- fold.vec == test.fold
+table( is.train )
 is.train <- !is.test
-x_train <- zip.X.array[is.train, , ,]
+x_train <- array(zip.X.array[is.train, , ,] , c(5832 , 16, 16 ,1 ))
 y_train <- zip.y.mat[is.train, ]
-x_test <- zip.X.array[is.test, , , ] 
+x_test <- array(zip.X.array[is.test, , , ] , c( 1459, 16 , 16 , 1))
 y_test <- zip.y.mat[is.test, ]
 
 test <- array(x_test, c(1459, 16, 16, 1))
 
 
-##conv.units <- c(784,6272,9216,128,10)
-##dense.units <- c(784,270,270,128,10)
+##conv.units <- c(256,6272,9216,128,10)
+##dense.units <- c(256,270,270,128,10)
 
   
 ## convolution
@@ -56,26 +57,23 @@ model %>% compile(
   )
   
 conv_result <- model %>% fit(
-  test, y_test,
+  x_train, y_train,
   batch_size = batch_size,
   epochs = epochs,
   validation_split = 0.2
   )
 
-some <- result$val_loss
-
-scores <- model %>% evaluate(
-  test, y_test, verbose = 0
-)
-
 conv_metrics.wide <- do.call(data.table::data.table, conv_result$metrics)
 conv_metrics.wide[, epoch := 1:.N]  
 str(conv_metrics.wide)
 
+conv_best_epoch = which.min(conv_metrics.wide$val_loss)
+
+
 ## dense
 
 model <- keras_model_sequential() %>%
-  layer_flatten( input_shape = c(16,16)) %>%                                 # input layer
+  layer_flatten( input_shape = dim(zip.X.array)[-1]) %>%                                 # input layer
   layer_dense(units = 270 , activation = "sigmoid" , use_bias = FALSE ) %>%  # hidden layer
   layer_dense(units = 270 , activation = "sigmoid" , use_bias = FALSE ) %>%  # hidden layer
   layer_dense(units = 128 , activation = "sigmoid" , use_bias = FALSE ) %>%  # hidden layer
@@ -96,6 +94,91 @@ dense_result <- model %>%
     verbose = 2
   )
 
-conv_metrics.wide <- do.call(data.table::data.table, conv_result$metrics)
-conv_metrics.wide[, epoch := 1:.N]  
-str(conv_metrics.wide)
+dense_metrics.wide <- do.call(data.table::data.table, dense_result$metrics)
+dense_metrics.wide[, epoch := 1:.N]  
+str(dense_metrics.wide)
+
+dense_best_epoch = which.min(dense_metrics.wide$val_loss)
+
+
+
+### Refitting according to best epochs
+
+## Convolutional
+conv_model <- keras_model_sequential() %>%
+  layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = 'relu',
+                input_shape = dim(zip.X.array)[-1]) %>% 
+  layer_conv_2d(filters = 64, kernel_size = c(3,3), activation = 'relu') %>% 
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
+  layer_flatten() %>% 
+  layer_dense(units = 128, activation = 'relu') %>%
+  layer_dense(units = num_classes, activation = 'softmax')
+
+conv_model %>% compile(
+  loss = loss_categorical_crossentropy,#for multi-class classification
+  optimizer = optimizer_adadelta(),
+  metrics = c('accuracy')
+)
+
+conv_result <- conv_model %>% fit(
+  x_train, y_train,
+  batch_size = batch_size,
+  epochs = conv_best_epoch,
+  validation_split = 0
+)
+
+conv_test_metrics.wide <- do.call(data.table::data.table, conv_result$metrics)
+conv_test_metrics.wide[, epoch := 1:.N]  
+str(conv_test_metrics.wide)
+
+## Dense
+
+dense_model <- keras_model_sequential() %>%
+  layer_flatten( input_shape = dim(zip.X.array)[-1]) %>%                                 # input layer
+  layer_dense(units = 270 , activation = "sigmoid" , use_bias = FALSE ) %>%  # hidden layer
+  layer_dense(units = 270 , activation = "sigmoid" , use_bias = FALSE ) %>%  # hidden layer
+  layer_dense(units = 128 , activation = "sigmoid" , use_bias = FALSE ) %>%  # hidden layer
+  layer_dense(units= 10 , activation = "softmax", use_bias = FALSE )         # ouput layer
+
+dense_model %>% 
+  compile(
+    loss = loss_categorical_crossentropy,
+    optimizer = "SGD",
+    metrics = "accuracy"
+  )
+
+dense_result <- dense_model %>% 
+  fit(
+    x = x_train, y = y_train,
+    epochs = dense_best_epoch,
+    validation_split = 0,
+    verbose = 2
+  )
+
+dense_test_metrics.wide <- do.call(data.table::data.table, dense_result$metrics)
+dense_test_metrics.wide[, epoch := 1:.N]  
+str(dense_test_metrics.wide)
+
+
+### Predict on Test data
+
+test_class <- apply(y_test, 1, which.max)
+
+conv_pred <- predict( conv_model, x_test)
+conv_pred_class <- apply(conv_pred, 1, which.max)
+conv_accuracy.vec <- conv_pred_class == test_class
+conv_accuracy <- sum(conv_accuracy.vec)
+
+dense_pred <- predict( dense_model , x_test)
+dense_pred_class <- apply(dense_pred, 1, which.max)
+dense_accuracy.vec <- dense_pred_class == test_class
+dense_accuracy <- sum(dense_accuracy.vec)
+
+# baseline accuracy
+baseline <- sum(1 == test_class)  ## which.max(zip.class.tab)
+
+
+
+
+
+
